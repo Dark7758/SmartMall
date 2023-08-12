0e04c587df7550c0052fe65cfa9935d9fbbf425e
尧诚 <yanbo.hh@foxmail.com>
2023-08-12 20:57:28 +0800

    demo

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// import '@/utils/vant-ui'
+import { Button, Icon } from 'vant'
+
+Vue.use(Button)
+Vue.use(Icon)
 
 Vue.config.productionTip = false
 
